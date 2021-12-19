3a552e6ae44330eceaa47ec720b88f8a966be68a
zdezak <zdezarus@gmail.com>
2021-12-19 21:01:45 +0500

    add retrofit and ApiService

---
+++ app/src/main/java/com/zdez/getinfoaboutalbums/lastfmapi/ApiServiceLastFM.kt
@@ -0,0 +1,43 @@
+package com.zdez.getinfoaboutalbums.lastfmapi
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.zdez.getinfoaboutalbums.dataclasses.ListOfAlbums
+import com.zdez.getinfoaboutalbums.dataclasses.ListOfArtists
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://ws.audioscrobbler.com/"
+
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ApiServiceLastFM {
+    @GET("2.0/")
+    fun searchArtist(
+        @Query("method") method: String = "artist.search",
+        @Query("artist") artist: String,
+        @Query("api_key") api_key: String,
+        @Query("format") format: String = "json"
+    ): Call<ListOfArtists>
+
+    @GET("2.0/?method=artist.getTopAlbums")
+    fun searchAlbums(
+        @Query("artist") artist: String,
+        @Query("mbid") mbid: String,
+        @Query("api_key") api_key: String,
+        @Query("format") format: String = "json"
+    ): Call<ListOfAlbums>
+}
+object ApiLastFm{
+    val retrofitService: ApiServiceLastFM by lazy {
+        retrofit.create(ApiServiceLastFM::class.java)
+    }
+}
