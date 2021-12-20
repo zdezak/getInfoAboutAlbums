70756a433e2a0efa946d7a58ae2ed0f19e72f220
zdezak <zdezarus@gmail.com>
2021-12-20 22:36:56 +0500

    add show result on AlbumsScreen

--- app/src/main/java/com/zdez/getinfoaboutalbums/screen/AlbumsScreen.kt
+++ app/src/main/java/com/zdez/getinfoaboutalbums/screen/AlbumsScreen.kt
@@ -1,18 +1,33 @@
 package com.zdez.getinfoaboutalbums.screen
 
+
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.accompanist.glide.rememberGlidePainter
+import com.zdez.getinfoaboutalbums.viewmodels.AlbumsViewModel
 
 @ExperimentalFoundationApi
 @Composable
-fun AlbumsScreen(artistName: String, navController: NavController) {
+fun AlbumsScreen(
+    artistName: String,
+    navController: NavController,
+    viewModel: AlbumsViewModel = AlbumsViewModel(artistName, ""),
+) {
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "List of albums") },
             navigationIcon = {
@@ -28,11 +43,33 @@ fun AlbumsScreen(artistName: String, navController: NavController) {
     }
     ) {
         LazyVerticalGrid(
-            cells = GridCells.Adaptive(minSize = 128.dp)
+            cells = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize()
+                .padding(16.dp)
         ) {
-//            items(photos) { photo ->
-//                PhotoItem(photo)
-//            }
+            items(viewModel.albums) { album ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .border(1.dp, color = Color.Black)
+                        .padding(6.dp)
+                ) {
+                    if (album.images[2].text.isEmpty()) {
+                        Text(
+                            text = "No image",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    } else {
+                        Image(
+                            painter = rememberGlidePainter(album.images[2].text),
+                            contentDescription = album.name,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/zdez/getinfoaboutalbums/viewmodels/AlbumsViewModel.kt
+++ app/src/main/java/com/zdez/getinfoaboutalbums/viewmodels/AlbumsViewModel.kt
@@ -1,8 +1,9 @@
 package com.zdez.getinfoaboutalbums.viewmodels
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.zdez.getinfoaboutalbums.BuildConfig
 import com.zdez.getinfoaboutalbums.dataclasses.album.Album
@@ -12,24 +13,26 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class AlbumsViewModel: ViewModel() {
+class AlbumsViewModel(artist: String, artistId: String) : ViewModel() {
     private val apiKey = BuildConfig.API_KEY
-    private val _albums = MutableLiveData<List<Album>>()
-    val albums: LiveData<List<Album>>
-        get() = _albums
+    var albums by mutableStateOf(listOf<Album>())
+        private set
 
-    fun getAlbums(artist: String, artistId: String) {
+    init {
+        getAlbums(artist = artist, artistId = artistId)
+    }
+    private fun getAlbums(artist: String, artistId: String) {
         ApiLastFm.retrofitService.searchAlbums(artist = artist, mbid = artistId, api_key = apiKey)
             .enqueue(object : Callback<ListOfAlbums> {
                 override fun onResponse(
                     call: Call<ListOfAlbums>,
                     response: Response<ListOfAlbums>,
                 ) {
-                    _albums.value = response.body()?.topAlbums?.albums
+                    albums = response.body()?.topAlbums?.albums ?: listOf()
                 }
 
                 override fun onFailure(call: Call<ListOfAlbums>, t: Throwable) {
-                    _albums.value = listOf()
+                    albums = listOf()
                     Log.i("albumsFail", t.message.toString())
                 }
             })
